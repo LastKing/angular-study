108b2ba113c126bbf88cbe604dd8d0ee5597416c
李太新 <toonew@danwi.me>
2016-01-07 18:56:44 +0800

    图片指令。。

---
+++ picture/picture-directive.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Rain on 2016/1/7.
+ */
+root.directive('picCutDir', function () {
+  return {
+    scope: {},
+    templateUrl: 'picture-directive.html',
+    compile: function (scope, element, attr) {
+
+    }
+  }
+});
